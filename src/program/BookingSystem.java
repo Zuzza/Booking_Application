6b3f3ec1e9fb351c3b7b83e98d79fcd9837a38af
Zuzza <zuzanna.kanafa@gmail.com>
2019-08-13 13:36:40 +1000

    Create BookingSystem.java

---
+++ src/program/BookingSystem.java
@@ -0,0 +1,16 @@
+
+package program;
+
+import Classes.Apartment;
+import gui.MainMenu;
+import java.util.ArrayList;
+
+/**
+ * Entry point for Booking system application
+ * @author Zuzanna Kanafa
+ */
+public class BookingSystem {
+    public static void main(String[] args) {
+        MainMenu app = new MainMenu();
+    }
+}
